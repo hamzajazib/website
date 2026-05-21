38fb93174e09280990e19dfd19e401565d585ba7
Yugi-2 <127689107+Yugi-2@users.noreply.github.com>
2026-05-21 12:47:44 -0400

    config change - reduce search from 100 to 50 results (#172)
    Co-authored-by: yug <>

--- application/config/params.php
+++ application/config/params.php
@@ -5,5 +5,5 @@ return [
     'classyCampaignId' => $parameters['classy_campaign_id'],
     'cacheTTL' => (array_key_exists('cacheTTL', $parameters) ? $parameters['cacheTTL'] : 3600),
     'searchCacheTTL' => (array_key_exists('searchCacheTTL', $parameters) ? $parameters['searchCacheTTL'] : 3600),
-    'pageSize' => '100',
+    'pageSize' => '50',
 ];
